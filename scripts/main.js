b81b8a978c2345b20e83e0b8afdf8aa4186bddf8
camelStyleUser <124447863+camelStyleUser@users.noreply.github.com>
2024-01-30 17:08:09 +0200

    tpyo

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let version="0.0";
 Events.on(EventType.ClientLoadEvent, () => {
     version=Vars.mods.locateMod("popup").meta.version;
     Log.info(version);
-    Http.get("https://raw.githubusercontent.com/camelStyleUser/popup-unforked/main/mod.json",(response)=>{if(JSON.parse(responce.getResultAsString()).version!=version){
+    Http.get("https://raw.githubusercontent.com/camelStyleUser/popup-unforked/main/mod.json",(response)=>{if(JSON.parse(response.getResultAsString()).version!=version){
         let updateDialog= new BaseDialog("update your popups");
         updateDialog.cont.add("update your popups").row();
         updateDialog.cont.button("update",()=>{
